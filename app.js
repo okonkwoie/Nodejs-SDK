f9dbcff8394c4e03b267a0cc4c3e87d080e9fd84
ijeoma <oijeoma11@gmail.com>
2023-04-04 15:35:07 +0100

    qr code successfully generating

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require("express");
-const { promisify } = require('util');
-const exec = promisify(require('child_process').exec);
+const { promisify } = require("util");
+const exec = promisify(require("child_process").exec);
 const config = require("./config/config");
-const fetch = require('node-fetch')
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 const bodyParser = require("body-parser");
 const cloudinary = require("cloudinary").v2;
 const app = express();
@@ -35,32 +35,66 @@ app.post("/qrcode", (req, res) => {
     return res.status(400).json({ message: "Missing file extension" });
   }
 
-  
   // Generate QR code using QR.io API
-  const qrCodeUrl = `https://qr.io/api/${type}?size=500&data=${encodeURIComponent(data)}`
+const qrCodeUrl = "https://api.qr.io/v1/create";
+const requestBody = {
+  apikey: config.API_KEY,
+  data,
+  transparent: "on",
+  frontcolor: "#000000",
+  marker_out_color: "#000000",
+  marker_in_color: "#000000",
+  pattern: "default",
+  marker: "default",
+  marker_in: "default",
+  optionlogo: "none"
+};
+const requestOptions = {
+  method: "POST",
+  headers: { "Content-Type": "application/json" },
+  body: JSON.stringify(requestBody)
+};
 
-  fetch(qrCodeUrl)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Failed to generate QR code');
-      }
-      return response.buffer();
-    })
+fetch(qrCodeUrl, requestOptions)
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Failed to generate QR code");
+    }
+    return response.arrayBuffer();
+  })
     .then(() => {
-      // Upload QR code to Cloudinary using Golang SDK
-      const fileName = `${type}_${data.replace(/[^\w\s]/gi, '')}_${file_extension}`;
-      return exec(`cloudinary-go upload --public-id ${fileName} --resource-type image ${fileName}`, { cwd: './' });
-    })
-    // returns url link to client 
-    .then((fileName) => {
-      const cloudinaryUrl = `https://res.cloudinary.com/${config.CLOUD_NAME}/image/upload/${fileName}`;
-      return res.json({ url: cloudinaryUrl, file_name: fileName });
-    })
-    .catch(err => {
-      console.log(err);
-      return res.status(500).json({ message: 'Failed to generate/upload QR code' });
-    });
-})
+        // Add success message
+        return res.json({ message: "QR code generated successfully" });
+      })
+      .catch((err) => {
+        console.log(err);
+        return res
+          .status(500)
+          .json({ message: "Failed to generate QR code" });
+      })
+    // .then(() => {
+    //   // Upload QR code to Cloudinary using Golang SDK
+    //   const fileName = `${type}_${data.replace(
+    //     /[^\w\s]/gi,
+    //     ""
+    //   )}_${file_extension}`;
+    //   return exec(
+    //     `cloudinary-go upload --public-id ${fileName} --resource-type image ${fileName}`,
+    //     { cwd: "./" }
+    //   );
+    // })
+    // // returns url link to client
+    // .then((fileName) => {
+    //   const cloudinaryUrl = `https://res.cloudinary.com/${config.CLOUD_NAME}/image/upload/${fileName}`;
+    //   return res.json({ url: cloudinaryUrl, file_name: fileName });
+    // })
+    // .catch((err) => {
+    //   console.log(err);
+    //   return res
+    //     .status(500)
+    //     .json({ message: "Failed to upload QR code" });
+    // });
+});
 
 // function to validate URL
 function isValidUrl(url) {
